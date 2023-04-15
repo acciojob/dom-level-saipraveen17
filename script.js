7b7455b5d98e8e047ac32595ea72bfb0f04d0ca7
Praveen Katikireddy <praveenk9417@gmail.com>
2023-04-15 18:06:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let node = document.getElementById("level");
+let level = 0;
+while (node.parentNode !== document.body) {
+  node = node.parentNode;
+  level++;
+}
+alert("The level of the element is: " + level);
